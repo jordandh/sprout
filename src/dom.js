8a6f5b1255d0790b600d632cde97e18f102950ec
Jordan Harrison <jordandh@gmail.com>
2012-07-15 15:17:40 -0700

    Added documentation to dom.js.

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,25 @@
-define(["jquery"], function ($) {
-    "use strict";
+define(['jquery'], function ($) {
+    'use strict';
 
-    // TODO: unit test this function and document it
-    $.containsComment = function(a, b)
+    /**
+     * @class dom
+     * Provides functionality for interacting with the dom. This module is jquery with extra functionality added on.
+     * @singleton
+     */
+    /**
+     * Returns whether or not a comment element is inside the node tree of another element.
+     * @param {Object} element The element to check inside of.
+     * @param {Object} comment The comment element to check for.
+     * @return {Boolean} Returns true if the comment element is in the node tree of the element.
+     */
+    $.containsComment = function(element, comment)
     {
-		while ( (b = b.parentNode) ) {
-			if ( b === a ) {
+		while ( (comment = comment.parentNode) ) {
+			if (comment === element) {
 				return true;
 			}
 		}
+		
 		return false;
 	};
     
