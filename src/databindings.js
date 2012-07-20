0cd36f64a8263919fd3c4dc8085d0c23a20704a3
Jordan Harrison <jordandh@gmail.com>
2012-07-20 11:39:22 -0700

    Fixed a bug in the foreach binder remove event handler where it wasn't removing all the dom elements for a removed item.

--- src/databindings.js
+++ src/databindings.js
@@ -156,13 +156,19 @@ define(["sprout/util", "sprout/dom"], function (_, $) {
 
         // If there is a list view then remove each model from it
         _.each(e.info.items, function (model) {
-            var cid = model.get("cid");
+            var cid = model.get("cid"),
+                encounteredItemsElements = false;
 
             // Remove the item from the dom
             itemElements.each(function () {
+                // If this element belongs to the item
                 if (this[foreachExpando] === cid) {
+                    encounteredItemsElements = true;
                     databindings.databind.removeBindings(this);
                     $(this).remove();
+                }
+                // Else if this element is for a different item and the item's elements have already been encountered (and removed) then we are all done
+                else if (encounteredItemsElements) {
                     return false;
                 }
             });
