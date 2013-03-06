b4c0b7a5d834e66329bde6111c1bb710a56b2c42
Jordan Harrison <jordandh@gmail.com>
2013-03-06 15:29:48 -0800

    Removed use of decycle in packaging error data. Added simple circular ref check to packaging error data that can handle deep graphs.

--- src/error.js
+++ src/error.js
@@ -1,4 +1,4 @@
-define(["sprout/pubsub", "sprout/util", "sprout/dom"], function (pubsub, _, $) {
+define(["sprout/pubsub", "sprout/util", "sprout/dom", "sprout/cereal"], function (pubsub, _, $, cereal) {
     "use strict";
 
     var errorModule = {
@@ -10,89 +10,107 @@ define(["sprout/pubsub", "sprout/util", "sprout/dom"], function (pubsub, _, $) {
         },
 
         stringify: function (error) {
-            return JSON.stringify(decycle(packageError(error)), jsonReplacer);
+            //return JSON.stringify(decycle(packageError(error)), jsonReplacer);
+            //return cereal.stringify(packageError(error));
+
+            return JSON.stringify(packageError(error), getSerialize(jsonReplacer));
         }
     };
 
-    /*
-     * Crockford's decycle
-     */
-    function decycle (object) {
-// Make a deep copy of an object or array, assuring that there is at most
-// one instance of each object or array in the resulting structure. The
-// duplicate references (which might be forming cycles) are replaced with
-// an object of the form
-//      {$ref: PATH}
-// where the PATH is a JSONPath string that locates the first occurance.
-// So,
-//      var a = [];
-//      a[0] = a;
-//      return JSON.stringify(JSON.decycle(a));
-// produces the string '[{"$ref":"$"}]'.
+//     /*
+//      * Crockford's decycle
+//      */
+//     function decycle (object) {
+// // Make a deep copy of an object or array, assuring that there is at most
+// // one instance of each object or array in the resulting structure. The
+// // duplicate references (which might be forming cycles) are replaced with
+// // an object of the form
+// //      {$ref: PATH}
+// // where the PATH is a JSONPath string that locates the first occurance.
+// // So,
+// //      var a = [];
+// //      a[0] = a;
+// //      return JSON.stringify(JSON.decycle(a));
+// // produces the string '[{"$ref":"$"}]'.
 
-// JSONPath is used to locate the unique object. $ indicates the top level of
-// the object or array. [NUMBER] or [STRING] indicates a child member or
-// property.
+// // JSONPath is used to locate the unique object. $ indicates the top level of
+// // the object or array. [NUMBER] or [STRING] indicates a child member or
+// // property.
 
-        var objects = [],   // Keep a reference to each unique object or array
-            paths = [];     // Keep the path to each unique object or array
+//         var objects = [],   // Keep a reference to each unique object or array
+//             paths = [];     // Keep the path to each unique object or array
 
-        return (function derez(value, path) {
+//         return (function derez(value, path) {
 
-// The derez recurses through the object, producing the deep copy.
+// // The derez recurses through the object, producing the deep copy.
 
-            var i,          // The loop counter
-                name,       // Property name
-                nu;         // The new object or array
+//             var i,          // The loop counter
+//                 name,       // Property name
+//                 nu;         // The new object or array
 
-// typeof null === 'object', so go on if this value is really an object but not
-// one of the weird builtin objects.
+// // typeof null === 'object', so go on if this value is really an object but not
+// // one of the weird builtin objects.
 
-            if (typeof value === 'object' && value !== null &&
-                    !(value instanceof Boolean) &&
-                    !(value instanceof Date)    &&
-                    !(value instanceof Number)  &&
-                    !(value instanceof RegExp)  &&
-                    !(value instanceof String)) {
+//             if (typeof value === 'object' && value !== null &&
+//                     !(value instanceof Boolean) &&
+//                     !(value instanceof Date)    &&
+//                     !(value instanceof Number)  &&
+//                     !(value instanceof RegExp)  &&
+//                     !(value instanceof String)) {
 
-// If the value is an object or array, look to see if we have already
-// encountered it. If so, return a $ref/path object. This is a hard way,
-// linear search that will get slower as the number of unique objects grows.
+// // If the value is an object or array, look to see if we have already
+// // encountered it. If so, return a $ref/path object. This is a hard way,
+// // linear search that will get slower as the number of unique objects grows.
 
-                for (i = 0; i < objects.length; i += 1) {
-                    if (objects[i] === value) {
-                        return {$ref: paths[i]};
-                    }
-                }
+//                 for (i = 0; i < objects.length; i += 1) {
+//                     if (objects[i] === value) {
+//                         return {$ref: paths[i]};
+//                     }
+//                 }
 
-// Otherwise, accumulate the unique value and its path.
+// // Otherwise, accumulate the unique value and its path.
 
-                objects.push(value);
-                paths.push(path);
+//                 objects.push(value);
+//                 paths.push(path);
 
-// If it is an array, replicate the array.
+// // If it is an array, replicate the array.
 
-                if (Object.prototype.toString.apply(value) === '[object Array]') {
-                    nu = [];
-                    for (i = 0; i < value.length; i += 1) {
-                        nu[i] = derez(value[i], path + '[' + i + ']');
-                    }
-                } else {
+//                 if (Object.prototype.toString.apply(value) === '[object Array]') {
+//                     nu = [];
+//                     for (i = 0; i < value.length; i += 1) {
+//                         nu[i] = derez(value[i], path + '[' + i + ']');
+//                     }
+//                 } else {
 
-// If it is an object, replicate the object.
+// // If it is an object, replicate the object.
 
-                    nu = {};
-                    for (name in value) {
-                        if (Object.prototype.hasOwnProperty.call(value, name)) {
-                            nu[name] = derez(value[name],
-                                path + '[' + JSON.stringify(name) + ']');
-                        }
-                    }
-                }
-                return nu;
-            }
-            return value;
-        }(object, '$'));
+//                     nu = {};
+//                     for (name in value) {
+//                         if (Object.prototype.hasOwnProperty.call(value, name)) {
+//                             nu[name] = derez(value[name],
+//                                 path + '[' + JSON.stringify(name) + ']');
+//                         }
+//                     }
+//                 }
+//                 return nu;
+//             }
+//             return value;
+//         }(object, '$'));
+//     }
+
+    function getSerialize (fn) {
+      var seen = [];
+      return function (key, value) {
+        var ret = value;
+        if (typeof value === 'object' && value) {
+          if (seen.indexOf(value) !== -1)
+            ret = '[Circular]';
+          else
+            seen.push(value);
+        }
+        if (fn) ret = fn(key, ret);
+        return ret;
+      };
     }
 
     function jsonReplacer (key, value)
