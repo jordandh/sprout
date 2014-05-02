4e1e56fe7af354ae3a44281e52526c464c758040
Jordan Harrison <jordandh@gmail.com>
2014-05-02 15:20:15 -0700

    Added the selectText function to the dom module which will select the text of an element.

--- src/dom.js
+++ src/dom.js
@@ -51,6 +51,29 @@ define(['jquery'], function ($) {
 
         return attributes;
 	};
+
+	/**
+	 * Selects the text of the first element.
+	 */
+	$.fn.selectText = function(){
+		var element = this[0],
+			range, selection;
+		
+		if (document.body.createTextRange) {
+			range = document.body.createTextRange();
+			range.moveToElementText(element);
+			range.select();
+		}
+		else if (window.getSelection) {
+			selection = window.getSelection();
+			range = document.createRange();
+			range.selectNodeContents(element);
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
+
+		return this;
+	};
     
     return $;
 });
